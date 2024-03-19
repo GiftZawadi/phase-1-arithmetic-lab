a2db65ebbd81eb93c23467185d234770398a2a21
Your Full Name <gzomandi07@gmail.com>
2024-03-19 11:09:46 +0300

    finish lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) +1;
+console.log(random)
+
+let num3 = 24;
+let num4 = 5;
+
+var mod = num3 % num4;
+
+var max = Math.max(10, 5, 20, 15);
